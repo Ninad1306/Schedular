a684b3f07c4a1151024456f596df515e050269e2
Ninad Parikh <parikhninad613@gmail.com>
2024-01-29 10:19:09 +0530

    perf: code cleaning, and performance improvement.

--- schedular/employee_management/doctype/employee/employee.js
+++ schedular/employee_management/doctype/employee/employee.js
@@ -3,41 +3,28 @@
 
 frappe.ui.form.on("Employee", {
     // Changes full name when first name changes
-    first_name: function(frm){
-        if (frm.doc.middle_name.trim().length > 0) {
-            frm.set_value('full_name', frm.doc.first_name  + ' ' +frm.doc.middle_name.toUpperCase()[0] + ' ' + (frm.doc.last_name || ''));
-        } 
-        else{
-            frm.set_value('full_name', frm.doc.first_name + ' ' + (frm.doc.last_name || ''));
-        }
-        
+    first_name: function (frm) {
+        frm.set_value('full_name', frm.doc.first_name + ' ' + (frm.doc.middle_name.trim()[0] || '') + ' ' + (frm.doc.last_name || ''));
     },
     // Changes full name when middle name changes
     middle_name: function (frm) {
         if (frm.doc.middle_name.trim().length > 0) {
-            frm.set_value('full_name', (frm.doc.first_name || '') + ' ' +frm.doc.middle_name.toUpperCase()[0] + ' ' + (frm.doc.last_name || ''));
+            frm.set_value('full_name', (frm.doc.first_name || '') + ' ' + frm.doc.middle_name.trim()[0] + ' ' + (frm.doc.last_name || ''));
         }
     },
     // Changes full name when last name changes
-    last_name: function(frm){
-        if (frm.doc.middle_name.trim().length > 0) {
-            frm.set_value('full_name', (frm.doc.first_name || '') + ' ' +frm.doc.middle_name.toUpperCase()[0] + ' ' + frm.doc.last_name);
-        }
-        else{
-            frm.set_value('full_name', (frm.doc.first_name || '') + ' ' + frm.doc.last_name);
-        }
-        
+    last_name: function (frm) {
+        frm.set_value('full_name', (frm.doc.first_name || '') + ' ' + (frm.doc.middle_name.trim()[0] || '') + ' ' + frm.doc.last_name);
     },
 
     // Based on the dob the age field is set.
-    dob: function (frm){
-        var dob = frappe.datetime.str_to_obj(frm.doc.dob);
+    dob: function (frm) {
         var today = frappe.datetime.now_datetime();
-        var age = frappe.datetime.get_diff(today, dob);
+        var age = frappe.datetime.get_diff(today, frm.doc.dob);
         var ageInYears = Math.floor(age / 365);
         frm.set_value('age', ageInYears)
-        if (ageInYears<18){
-            frappe.throw('Enter valid date of birth.')
+        if (ageInYears < 18) {
+            frappe.throw('Employee age should be 18 or greater.')
         }
-	},
+    },
 });
--- schedular/task_management/doctype/task/task.py
+++ schedular/task_management/doctype/task/task.py
@@ -10,27 +10,21 @@ class Task(Document):
 	def before_save(self):
 		if self.stage == 'In Review':
 			self.send_email(self.task_name, self.task_desc)
-
-	# Stage is reverted to the last stage before the document is saved.
-	@frappe.whitelist()
-	def revert_stage(self):
-		old_doc = Document.get_doc_before_save(self)
-		return old_doc.stage
 	
 	# Email is sent to reviewer when stage is changed to 'In Review'.
 	def send_email(self, subject, desc):
-		emp_no = frappe.db.get_value('Employee',{'emp_role': 'Reviewer'}, ['emp_no'])
-		employee_doc = frappe.get_doc('Employee', emp_no)
-		frappe.sendmail(recipients=employee_doc.email, subject=subject, message= desc, now=True)
+		employee_email = frappe.db.get_value('Employee', {'emp_role': 'Reviewer'}, ['email'])
+		frappe.sendmail(recipients=employee_email, subject=subject, message= desc, now=True)
 	
 	def validate(self):
-		if self.due_date < today():
+		old_task_doc = self.get_doc_before_save()
+
+		if self.due_date!=old_task_doc.due_date and self.due_date<today():
 			frappe.throw('Enter valid date.')
 		
-		old_doc = self.get_doc_before_save()
 		if self.stage == 'Done':
-			if old_doc.stage!='Done':
+			if old_task_doc.stage!='Done':
 				self.completed_on_date = today()
 			else:
-				self.completed_on_date = old_doc.completed_on_date
+				self.completed_on_date = old_task_doc.completed_on_date
 		
